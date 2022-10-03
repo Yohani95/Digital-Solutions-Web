a92a9029b2158aea116c1c7144382eed6113c6e0
Yohani Espinoza <yohani95301@gmail.com>
2022-10-02 21:23:09 -0300

    avances

--- forms/EmailSend.php
+++ forms/EmailSend.php
@@ -8,7 +8,6 @@
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'digital.solutions.spa22@gmail.com';
-var_dump($_SERVER);
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
@@ -16,11 +15,9 @@ var_dump($_SERVER);
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
-  $msg=$_POST['message'];
-mail($contact->to,$contact->subject,$msg);
-//   $contact->add_message( $_POST['name'], 'From');
-//   $contact->add_message( $_POST['email'], 'Email');
-//   $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['name'], 'From');
+     $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
-//   echo $contact->send();
+  echo $contact->send();
 ?>
